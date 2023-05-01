6e46ce50ec9e7152e7d5f3be4e9edf7914298091
Yassine Elgharouji <yelgharo@e2r10p15.1337.ma>
2023-05-01 13:07:31 +0100

    add install docker-compose

--- install_docker_on_ubuntu_script.sh
+++ install_docker_on_ubuntu_script.sh
@@ -7,4 +7,4 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
